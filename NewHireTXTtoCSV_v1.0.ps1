356a6fc53b44283a23fd45bb68b64b08b9bc50d0
ChrisZ-IT <55855316+ChrisZ-IT@users.noreply.github.com>
2019-09-26 13:59:40 -0600

    Create NewHireTXTtoCSV_v1.0.ps1
    Script needs a txt named newhire.txt saved to your desktop with the following info.
    Username: Test User
    Title: Test Title
    Department: Test Department
    Manager: Test Manager
    Similar User: Similar user to copy perms from

---
+++ NewHireTXTtoCSV_v1.0.ps1
@@ -0,0 +1,9 @@
+### Script to convert a txt file to csv files to that can be imported by AD ###
+### Author: Chris-Z-IT ###
+### Version: 1.0 ###
+### Created 10.26.2017 ###
+
+
+$userObj = [pscustomobject] ((Get-Content -Raw $env:userprofile\Desktop\newhire.txt) -replace ':', '=' |
+ConvertFrom-StringData)
+$userObj | Select-Object Username, Title, Department, Manager, 'Similar User' | ConvertTo-Csv -NoTypeInformation | % {$_ -replace '"', ""} | Out-File $env:userprofile\Desktop\NewHireConvert.csv -Force -Encoding ascii -Append
