de35e4a227b5c77186f9cd40b047c113aa32d603
ChrisZ-IT <55855316+ChrisZ-IT@users.noreply.github.com>
2019-09-26 14:01:29 -0600

    Update NewHireTXTtoCSV_v1.0.ps1

--- NewHireTXTtoCSV_v1.0.ps1
+++ NewHireTXTtoCSV_v1.0.ps1
@@ -1,7 +1,7 @@
 ### Script to convert a txt file to csv files to that can be imported by AD ###
 ### Author: Chris-Z-IT ###
 ### Version: 1.0 ###
-### Created 10.26.2017 ###
+### Created 10.26.2016 ###
 
 
 $userObj = [pscustomobject] ((Get-Content -Raw $env:userprofile\Desktop\newhire.txt) -replace ':', '=' |
